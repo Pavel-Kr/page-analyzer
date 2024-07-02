1af563d30a514128e3fe25274a1b4b7c2ca1d4d7
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-07-02 22:40:26 +0700

    Changed autoincrement type for DB

--- database.sql
+++ database.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
     created_at DATE
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls(id),
     status_code INTEGER,
     h1 VARCHAR(255),
+    title VARCHAR(255),
     description TEXT,
     created_at DATE
 );
